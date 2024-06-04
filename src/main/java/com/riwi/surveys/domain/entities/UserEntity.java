e9614b916a160321fa524ebb4e876177996af1b2
LuisaP28 <151669754+LuisaP28@users.noreply.github.com>
2024-06-04 07:18:13 -0500

    Entidad terminada

--- src/main/java/com/riwi/surveys/domain/entities/UserEntity.java
+++ src/main/java/com/riwi/surveys/domain/entities/UserEntity.java
@@ -1,22 +1,32 @@
 package com.riwi.surveys.domain.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "user")
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String name;
+    @Column(length = 100, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
-    private Double active;
+    @Column(nullable = false)
+    private Boolean active;
 }
