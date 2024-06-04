c047996b284052fed05140b3b6ca57a310f08ba5
LuisaP28 <151669754+LuisaP28@users.noreply.github.com>
2024-06-04 07:37:07 -0500

    Entidad finalizada

--- src/main/java/com/riwi/surveys/domain/entities/SurveyEntity.java
+++ src/main/java/com/riwi/surveys/domain/entities/SurveyEntity.java
@@ -1,26 +1,45 @@
 package com.riwi.surveys.domain.entities;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "survey")
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SurveyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String description;
-    private Timestamp creationDate;
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
     private Long creatorId;
-    private Double active;
+    @Column(nullable = false)
+    private Boolean active;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserEntity userId;
+
+    @OneToMany(mappedBy = "surveyId", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = false)
+    private Question quiestionId;
 }
